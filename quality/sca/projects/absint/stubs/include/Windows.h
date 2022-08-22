929c3d7ade7b413d9fda66eeeabaa626a8093868
Sergey Baigudin <baigudin@mail.ru>
2022-08-22 22:01:54 +0300

    [EOOS-26] All SCA violation fixed

--- quality/sca/projects/absint/stubs/include/Windows.h
+++ quality/sca/projects/absint/stubs/include/Windows.h
@@ -71,27 +71,27 @@ typedef struct _SMALL_RECT {
 } SMALL_RECT;
 
 // #include <WinCon.h>
-#define FOREGROUND_BLUE 0x0001            // Text color contains blue.
-#define FOREGROUND_GREEN 0x0002           // Text color contains green.
-#define FOREGROUND_RED 0x0004             // Text color contains red.
-#define FOREGROUND_INTENSITY 0x0008       // Text color is intensified.
-#define BACKGROUND_BLUE 0x0010            // Background color contains blue.
-#define BACKGROUND_GREEN 0x0020           // Background color contains green.
-#define BACKGROUND_RED 0x0040             // Background color contains red.
-#define BACKGROUND_INTENSITY 0x0080       // Background color is intensified.
-#define COMMON_LVB_LEADING_BYTE 0x0100    // Leading byte.
-#define COMMON_LVB_TRAILING_BYTE 0x0200   // Trailing byte.
-#define COMMON_LVB_GRID_HORIZONTAL 0x0400 // Top horizontal.
-#define COMMON_LVB_GRID_LVERTICAL 0x0800  // Left vertical.
-#define COMMON_LVB_GRID_RVERTICAL 0x1000  // Right vertical.
-#define COMMON_LVB_REVERSE_VIDEO 0x4000   // Reverse foreground and background attribute.
-#define COMMON_LVB_UNDERSCORE 0x8000      // Underscore.
+#define FOREGROUND_BLUE 0x0001U            // Text color contains blue.
+#define FOREGROUND_GREEN 0x0002U           // Text color contains green.
+#define FOREGROUND_RED 0x0004U             // Text color contains red.
+#define FOREGROUND_INTENSITY 0x0008U       // Text color is intensified.
+#define BACKGROUND_BLUE 0x0010U            // Background color contains blue.
+#define BACKGROUND_GREEN 0x0020U           // Background color contains green.
+#define BACKGROUND_RED 0x0040U             // Background color contains red.
+#define BACKGROUND_INTENSITY 0x0080U       // Background color is intensified.
+#define COMMON_LVB_LEADING_BYTE 0x0100U    // Leading byte.
+#define COMMON_LVB_TRAILING_BYTE 0x0200U   // Trailing byte.
+#define COMMON_LVB_GRID_HORIZONTAL 0x0400U // Top horizontal.
+#define COMMON_LVB_GRID_LVERTICAL 0x0800U  // Left vertical.
+#define COMMON_LVB_GRID_RVERTICAL 0x1000U  // Right vertical.
+#define COMMON_LVB_REVERSE_VIDEO 0x4000U   // Reverse foreground and background attribute.
+#define COMMON_LVB_UNDERSCORE 0x8000U      // Underscore.
 
-#define STD_INPUT_HANDLE (4294967286-10)  // The standard input device. Initially, this is the console input buffer, CONIN$.
-#define STD_OUTPUT_HANDLE (4294967286-11) // The standard output device. Initially, this is the active console screen buffer, CONOUT$.
-#define STD_ERROR_HANDLE (4294967286-12)  // The standard error device. Initially, this is the active console screen buffer, CONOUT$.
+#define STD_INPUT_HANDLE (4294967286U-10U)  // The standard input device. Initially, this is the console input buffer, CONIN$.
+#define STD_OUTPUT_HANDLE (4294967286U-11U) // The standard output device. Initially, this is the active console screen buffer, CONOUT$.
+#define STD_ERROR_HANDLE (4294967286U-12U)  // The standard error device. Initially, this is the active console screen buffer, CONOUT$.
 
-#define INVALID_HANDLE_VALUE 0
+#define INVALID_HANDLE_VALUE ( (void*)(-1) )
 
 // #include <ConsoleApi2.h>
 typedef struct _CONSOLE_SCREEN_BUFFER_INFO {
