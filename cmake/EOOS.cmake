d94e42e237a567dcbc8415dc82122abdefa0f2e3
Sergey Baigudin <baigudin@mail.ru>
2023-04-16 21:05:07 +0300

    [EOOS-37] CMake setup for no heap enabling

--- cmake/EOOS.cmake
+++ cmake/EOOS.cmake
@@ -1,6 +1,8 @@
 # EOOS WIN32 OPTIONS.
 #
 # @author    Sergey Baigudin, sergey@baigudin.software
-# @copyright 2020-2022, Sergey Baigudin, Baigudin Software
+# @copyright 2020-2023, Sergey Baigudin, Baigudin Software
 
-option(EOOS_ENABLE_TESTS "Build unit test with the project" OFF)
+set(EOOS_CMAKE_ENABLE_TESTS OFF CACHE BOOL "Enable building unit test")
+
+set(EOOS_GLOBAL_ENABLE_NO_HEAP OFF CACHE BOOL "Enable no using heap memory")
