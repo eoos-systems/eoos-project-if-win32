994cdd84e8c6668c66eb045f9e77a8034c24700a
Sergey Baigudin <baigudin@mail.ru>
2024-09-26 23:42:32 +0300

    [EOOS-50] Align with KasperskyOS

--- cmake/EOOS.cmake
+++ cmake/EOOS.cmake
@@ -4,5 +4,6 @@
 # @copyright 2020-2023, Sergey Baigudin, Baigudin Software
 
 set(EOOS_CMAKE_ENABLE_TESTS OFF CACHE BOOL "Enable building unit test")
+set(EOOS_CMAKE_TARGET_OS "WIN32" CACHE STRING "Define target OS for which EOOS is built")
 
 set(EOOS_GLOBAL_ENABLE_NO_HEAP OFF CACHE BOOL "Enable no using heap memory")
